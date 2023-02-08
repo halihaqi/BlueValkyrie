5d248afff3f1c5ab4bef22eed54a528109212047
halihaqi <1091315382@qq.com>
2023-02-08 21:23:54 +0800

    [init] 美术资源导入

---
+++ Assets/Scripts/Game/UI/Begin/BeginPanel.cs
@@ -0,0 +1,12 @@
+﻿using Hali_Framework;
+
+namespace Game.UI.Begin
+{
+    public class BeginPanel : PanelBase
+    {
+        protected internal override void OnShow(object userData)
+        {
+            base.OnShow(userData);
+        }
+    }
+}
